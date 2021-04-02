61163034fab94980e24748733be122f231549983
Leeeeeeeeeeeeeeeeeeeee <dimarodin0@gmail.com>
2021-04-02 17:37:07 +0400

    project structure, global naming

---
+++ src/r1engine.ts
@@ -0,0 +1,6 @@
+import { Entity } from "./entity/entity";
+
+export * as Collider from "./collider/collisions";
+export * as Entity from "./entity/entities";
+export * as Map from "./map/maps";
+export * as Keys from "./movement/arrows";
